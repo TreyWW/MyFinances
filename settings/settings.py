2ea4770da05ebf48c7d511b5783619210b016604
Trey <treyww2008@hotmail.com>
2023-08-20 21:43:18 +0100

    Trying to fix ENV not being found

--- settings/settings.py
+++ settings/settings.py
@@ -2,16 +2,14 @@ from pathlib import Path
 import os, mimetypes, json
 from django.contrib.messages import constants as messages
 
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
 DEBUG = True if os.environ.get("DEBUG") in ["True", "true", "TRUE", True] else False
 
 try:
     if DEBUG:
         print("[BACKEND] Using local settings")
         from .local_settings import *
+    elif os.environ.get("DEBUG") == "":
+        exit("[BACKEND] No ENV found, or debug not set.")
     else:
         print("[BACKEND] Using production settings")
         from .prod_settings.py import *
