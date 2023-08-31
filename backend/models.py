51785f06ccfe8b3eafbf08668194c76ed0a5f7ef
Trey <treyww2008@hotmail.com>
2023-08-31 18:01:33 +0100

    🖥 Added backend for creating invoices
    Todo:
    Toggle between service/product

--- backend/models.py
+++ backend/models.py
@@ -14,6 +14,7 @@ def RandomCode(length=6):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
 
+
 class UserSettings(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
     dark_mode = models.BooleanField(default=True)
@@ -28,6 +29,16 @@ class Client(models.Model):
         return self.name
 
 
+class InvoiceItem(models.Model):
+    description = models.CharField(max_length=100)
+    is_service = models.BooleanField(default=True)
+    # if service
+    hours = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
+    price_per_hour = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
+    # if product
+    price = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
+
+
 class Invoice(models.Model):
     STATUS_CHOICES = (
         ('pending', 'Pending'),
@@ -36,15 +47,15 @@ class Invoice(models.Model):
     )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    invoice_id = models.CharField(max_length=20, unique=True)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    services = models.TextField()
-    hourly_rate = models.DecimalField(max_digits=10, decimal_places=2)
-    hours_worked = models.DecimalField(max_digits=5, decimal_places=2)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    invoice_id = models.IntegerField(unique=True, blank=True, null=True)  # todo: add
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=True, null=True)  # todo: add
+
+    payment_status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    items = models.ManyToManyField(InvoiceItem)
+
     date_created = models.DateTimeField(auto_now_add=True)
     date_due = models.DateField()
-    payment_status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    date_issued = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"Invoice {self.invoice_id} for {self.client}"
@@ -55,7 +66,6 @@ class Invoice(models.Model):
             return int(amount)
         else:
             return amount
-
 
 
 class PasswordSecrets(models.Model):
