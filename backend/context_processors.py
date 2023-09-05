408825238887f63bcbb55ec0f3efa9334e5938e8
Trey <73353716+TreyWW@users.noreply.github.com>
2023-09-05 16:31:04 +0100

    Revert "Feature/invoices/39 create (#60)" (#61)
    This reverts commit 3ca43b66d929bb6bebf3f2e8576ec845bf176d9f.

--- backend/context_processors.py
+++ backend/context_processors.py
@@ -2,6 +2,7 @@ import json
 import os
 import subprocess
 from django.http import HttpRequest
+from django.urls import reverse
 
 from .utils import Notification, Toast, Modals, load_navbar_items
 from .models import *
@@ -17,8 +18,8 @@ def navbar(request):
     if cached_navbar_items is None:
         navbar_items = load_navbar_items()
 
-        # Cache the sidebar items for a certain time (e.g., 3600 seconds = 1 hr)
-        cache.set('navbar_items', navbar_items, 60 * 60 * 3)  # 3 hrs
+        # Cache the sidebar items for a certain time (e.g., 3600 seconds)
+        cache.set('navbar_items', navbar_items, 60 * 60 * 3)
     else:
         navbar_items = cached_navbar_items
     context = {"navbar_items": navbar_items}
@@ -30,6 +31,9 @@ def extras(request: HttpRequest):
 
     data['git_branch'] = os.environ.get('BRANCH')
     data['git_version'] = os.environ.get('VERSION')
+    data['modals'] = [
+        {"id": "logout_modal", "title": "Log out", "text": "Are you sure you would like to logout?", "action": {"text": "Log out", "type": "anchor", "href": reverse("logout")}}
+    ]
 
     return data
 
@@ -41,6 +45,7 @@ def toasts(request):
             'toasts': toasts,
         }
     return {}
+
 
 # def notifications(request: HttpRequest):
 #     if request.user.is_authenticated:
--- backend/views/core/invoices/create.py
+++ backend/views/core/invoices/create.py
@@ -1,42 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.shortcuts import render
-from django.views.decorators.http import require_http_methods
-from backend.models import Invoice, InvoiceItem, Client
 
 
 @login_required
-@require_http_methods(['GET', 'POST', 'DELETE'])
 def create_invoice_page(request: HttpRequest):
     if request.method == "POST":
-        return post(request)
-    return get(request)
+        for key, value in request.POST.items():
+            print('Key: %s' % (key))
+            # print(f'Key: {key}') in Python >= 3.7
+            print('Value:  %s' % (value))
 
-
-def get(request):
-    return render(request, "core/pages/invoices/create/create.html")
-
-
-def post(request):
-    context = {}
-
-    invoice_items = [
-        InvoiceItem.objects.create(
-            description = row[0],
-            hours = row[1],
-            price_per_hour = row[2]
-        ) for row in zip(
-            request.POST.getlist('service_name[]'),
-            request.POST.getlist('hours[]'),
-            request.POST.getlist('price_per_hour[]')
-        )
-    ] # Todo: add products to this for logic
-
-    invoice = Invoice.objects.create(
-        user = request.user,
-        date_due = request.POST.get('date_due'),
-        date_issued = request.POST.get('date_issued')
-    )
-
-    invoice.items.set(invoice_items)
+        print(f"list: {request.POST.getlist('good_name')}")
+        print(f"list 2: {request.POST.getlist('good_quantity')}")
     return render(request, "core/pages/invoices/create/create.html")
