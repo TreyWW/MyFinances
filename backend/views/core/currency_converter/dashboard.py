861685ffbbb515f2447b1e13bedd897e02c640e4
Matt Juarez <matthewjuarez1@mail.weber.edu>
2023-11-19 20:00:26 -0700

    reformatted currency_converter file with black

--- backend/views/core/currency_converter/dashboard.py
+++ backend/views/core/currency_converter/dashboard.py
@@ -1,5 +1,6 @@
 from forex_python.converter import CurrencyRates
 
+
 def convert_currency(init_currency, target_currency, amount):
     """
     Converts one currency to another, given an amount, using the forex_python library
@@ -8,13 +9,13 @@ def convert_currency(init_currency, target_currency, amount):
     ----------
     init_currency : str, required
         The code for the initial currency to be converted
-    
+
     target_currency : str, required
         The code for the target currency to be converted to
-    
+
     amount : int or float, required
         The amount to be converted
-    
+
     Returns
     ----------
     Returns an int or float representing the new amount
@@ -26,14 +27,12 @@ def convert_currency(init_currency, target_currency, amount):
     if not isinstance(target_currency, str) or len(target_currency) != 3:
         print("Target currency not recognized, returning original amount")
         return amount
-    if not isinstance(amount, int) or not isinstance(amount,float):
+    if not isinstance(amount, int) or not isinstance(amount, float):
         print("Amount is not an accepted datatype, returning original amount")
         return amount
 
     currency_rates = CurrencyRates()
 
-    target_amount = currency_rates.get_rates(init_currency, 
-                                             target_currency, 
-                                             amount)
-    
-    return round(target_amount,2)
+    target_amount = currency_rates.get_rates(init_currency, target_currency, amount)
+
+    return round(target_amount, 2)
