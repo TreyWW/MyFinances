3cf6d52422065f40568bdb072322346d0cbbe391
Trey <73353716+TreyWW@users.noreply.github.com>
2023-11-26 16:56:01 +0000

    #119 - Complete receipt modals and tests (#122)
    * Fixed date_uploaded (previous was updating the field on any modification)
    
    * Fixed date_uploaded (previous was updating the field on any modification)
    
    * Added get_receipt_url
    
    * Fixed #118
    
    * Added receipt modals and appropriate tests
    
    * Added message for when receipt gets deleted
    
    * Deletes mock images after tests are complete

--- tests/handler.py
+++ tests/handler.py
@@ -1,6 +1,24 @@
 from django.urls import reverse
 from django.test import TestCase
-from backend.models import User
+from backend.models import User, Receipt, UserSettings
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+from io import BytesIO
+
+
+def create_mock_image():
+    """
+    Create a simple mock image using Pillow.
+
+    Returns:
+        SimpleUploadedFile: A mock image as a SimpleUploadedFile.
+    """
+    image = Image.new("RGB", (100, 100), "white")
+    image_io = BytesIO()
+    image.save(image_io, "JPEG")
+    return SimpleUploadedFile(
+        "mock_image.jpg", image_io.getvalue(), content_type="image/jpeg"
+    )
 
 
 class ViewTestCase(TestCase):
@@ -8,6 +26,13 @@ class ViewTestCase(TestCase):
         self.log_in_user = User.objects.create_user(
             username="user@example.com", password="user", email="user@example.com"
         )
+        self.mock_images = []
+
+    def tearDown(self):
+        [image.delete() for image in self.mock_images]
+        [receipt.image.delete() for receipt in Receipt.objects.all()]
+        [pfp.profile_picture.delete() for pfp in UserSettings.objects.all()]
+        super().tearDown()
 
     def call_index(self):
         self.client.get(reverse("index"))
--- tests/views/test_receipts.py
+++ tests/views/test_receipts.py
@@ -1,5 +1,8 @@
+from django.contrib.messages import get_messages
 from django.urls import reverse, resolve
-from tests.handler import ViewTestCase
+
+from backend.models import Receipt
+from tests.handler import ViewTestCase, create_mock_image
 
 from model_bakery import baker
 
@@ -8,18 +11,22 @@ class ReceiptsViewTestCase(ViewTestCase):
     def setUp(self):
         super().setUp()
 
+        # Define URLs for the receipts dashboard
         self._receipts_dashboard_url = reverse("receipts dashboard")
 
     def test_receipts_dashboard_view_302_for_non_authenticated_users(self):
+        # Test that non-authenticated users are redirected
         response = self.client.get(self._receipts_dashboard_url)
         self.assertEqual(response.status_code, 302)
 
     def test_receipts_dashboard_view_200_for_authenticated_users(self):
+        # Test that authenticated users get a successful response
         self.login_user()
         response = self.client.get(self._receipts_dashboard_url)
         self.assertEqual(response.status_code, 200)
 
     def test_receipts_dashboard_view_matches_with_urls_view(self):
+        # Test that the URL resolves to the correct view function
         func = resolve("/dashboard/receipts").func
         func_name = f"{func.__module__}.{func.__name__}"
         self.assertEqual("/dashboard/receipts", self._receipts_dashboard_url)
@@ -32,18 +39,23 @@ class ReceiptsAPITestCase(ViewTestCase):
     def setUp(self):
         super().setUp()
 
+        # Define URLs for the receipts API
         self._receipts_api_fetch_url = reverse("api v1 receipts fetch")
+        self._receipts_api_create_url = reverse("api v1 receipts new")
 
     def test_receipts_fetch_api_view_302_for_non_authenticated_users(self):
+        # Test that non-authenticated users are redirected
         response = self.client.get(self._receipts_api_fetch_url)
         self.assertEqual(response.status_code, 302)
 
     def test_receipts_fetch_api_view_404_for_authenticated_users(self):
+        # Test that authenticated users get a 404 response
         self.login_user()
         response = self.client.get(self._receipts_api_fetch_url)
         self.assertEqual(response.status_code, 404)
 
     def test_receipts_fetch_api_view_matches_with_urls_view(self):
+        # Test that the URL resolves to the correct view function
         func = resolve("/api/v1/receipts/fetch").func
         func_name = f"{func.__module__}.{func.__name__}"
         self.assertEqual("/api/v1/receipts/fetch", self._receipts_api_fetch_url)
@@ -52,6 +64,7 @@ class ReceiptsAPITestCase(ViewTestCase):
         )
 
     def test_receipts_fetch_api_view_returns_correct_amount(self):
+        # Test that the API returns the correct number of receipts
         _quantity = 4
 
         baker.make(
@@ -67,6 +80,7 @@ class ReceiptsAPITestCase(ViewTestCase):
         self.assertEqual(len(response.context["receipts"]), 4)
 
     def test_receipts_fetch_api_view_search_text(self):
+        # Test searching for receipts with specific text
         search_text = "text"
 
         baker.make(
@@ -76,7 +90,7 @@ class ReceiptsAPITestCase(ViewTestCase):
             _quantity=1,
             _create_files=True,
         )
-        # not on search
+        # Not on search
         baker.make(
             "backend.Receipt",
             user=self.log_in_user,
@@ -91,3 +105,47 @@ class ReceiptsAPITestCase(ViewTestCase):
         response = self.client.get(self._receipts_api_fetch_url, data, **headers)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context["receipts"]), 1)
+
+    def test_receipt_create_post_with_valid(self):
+        # Test creating a receipt with valid data
+        self.login_user()
+        self.assertEqual(len(Receipt.objects.all()), 0)
+        # Create a mock receipt image
+        mock_image = create_mock_image()
+
+        data = {"receipt_name": "some name", "receipt_image": mock_image}
+        headers = {"HTTP_HX-Request": "true"}
+        response = self.client.post(self._receipts_api_create_url, data, **headers)
+        self.assertEqual(len(Receipt.objects.all()), 1)
+        self.assertIn("mock_image", Receipt.objects.first().image.name)
+        self.assertTemplateUsed(response, "pages/receipts/_search_results.html")
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), "Receipt added with the name of some name")
+
+    def test_receipt_create_post_with_valid_but_no_name(self):
+        # Test creating a receipt with a valid image but no name
+        self.login_user()
+        self.assertEqual(len(Receipt.objects.all()), 0)
+        # Create a mock receipt image
+        mock_image = create_mock_image()
+        data = {"receipt_name": "", "receipt_image": mock_image}
+        headers = {"HTTP_HX-Request": "true"}
+        response = self.client.post(self._receipts_api_create_url, data, **headers)
+        self.assertEqual(len(Receipt.objects.all()), 1)
+        self.assertTemplateUsed(response, "pages/receipts/_search_results.html")
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), "Receipt added with the name of mock_image")
+
+    def test_receipt_create_post_with_invalid_image(self):
+        # Test creating a receipt with an invalid image
+        self.login_user()
+        data = {"receipt_name": "some name", "receipt_image": "not a valid image"}
+        headers = {"HTTP_HX-Request": "true"}
+        response = self.client.post(self._receipts_api_create_url, data, **headers)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.content.decode("utf-8"), "No image found")
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), "No image found")
