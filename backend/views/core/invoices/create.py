f1d69172aaa84ef4eade77c4ebc93f41d290d53c
Strelix <treyww2008@hotmail.com>
2023-12-15 11:50:37 +0000

    Added ability to import existing clients, ability to add client as a representative of a company.

--- backend/views/core/invoices/create.py
+++ backend/views/core/invoices/create.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
@@ -32,30 +33,47 @@ def invoice_page_post(request: HttpRequest):
         user=request.user,
         date_due=datetime.strptime(request.POST.get("date_due"), "%Y-%m-%d").date(),
         date_issued=request.POST.get("date_issued"),
-        client_name=request.POST.get("to_name"),
-        client_company=request.POST.get("to_company"),
-        client_address=request.POST.get("to_address"),
-        client_city=request.POST.get("to_city"),
-        client_county=request.POST.get("to_county"),
-        client_country=request.POST.get("to_country"),
-        client_is_representative=True
-        if request.POST.get("is_representative") == "on"
-        else False,
-        self_name=request.POST.get("from_name"),
-        self_company=request.POST.get("from_company"),
-        self_address=request.POST.get("from_address"),
-        self_city=request.POST.get("from_city"),
-        self_county=request.POST.get("from_county"),
-        self_country=request.POST.get("from_country"),
-        notes=request.POST.get("notes"),
-        invoice_number=request.POST.get("invoice_number"),
-        vat_number=request.POST.get("vat_number"),
-        logo=request.FILES.get("logo"),
-        reference=request.POST.get("reference"),
-        sort_code=request.POST.get("sort_code"),
-        account_number=request.POST.get("account_number"),
-        account_holder_name=request.POST.get("account_holder_name"),
     )
+
+    is_existing_client = True if request.POST.get("selected_client") else False
+
+    if is_existing_client:
+        try:
+            client = Client.objects.get(
+                user=request.user, id=request.POST.get("selected_client")
+            )
+        except:
+            messages.error(request, "Client not found")
+            invoice.delete()
+            return render(request, "pages/invoices/create/create.html")
+        invoice.client_to = client
+
+    else:
+        invoice.client_name = (request.POST.get("to_name"),)
+        invoice.client_company = (request.POST.get("to_company"),)
+        invoice.client_address = (request.POST.get("to_address"),)
+        invoice.client_city = (request.POST.get("to_city"),)
+        invoice.client_county = (request.POST.get("to_county"),)
+        invoice.client_country = (request.POST.get("to_country"),)
+        invoice.client_is_representative = (
+            True if request.POST.get("is_representative") == "on" else False
+        )
+
+    invoice.self_name = request.POST.get("from_name")
+    invoice.self_company = request.POST.get("from_company")
+    invoice.self_address = request.POST.get("from_address")
+    invoice.self_city = request.POST.get("from_city")
+    invoice.self_county = request.POST.get("from_county")
+    invoice.self_country = request.POST.get("from_country")
+
+    invoice.notes = request.POST.get("notes")
+    invoice.invoice_number = request.POST.get("invoice_number")
+    invoice.vat_number = request.POST.get("vat_number")
+    invoice.logo = request.FILES.get("logo")
+    invoice.reference = request.POST.get("reference")
+    invoice.sort_code = request.POST.get("sort_code")
+    invoice.account_number = request.POST.get("account_number")
+    invoice.account_holder_name = request.POST.get("account_holder_name")
 
     invoice.payment_status = invoice.dynamic_payment_status
 
