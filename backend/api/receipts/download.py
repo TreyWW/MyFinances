3f3dc4156320ab00182203411c09fce25c526b4a
SharonAliyas5573 <sharonaliyas25@gmail.com>
2024-01-14 22:19:25 +0530

    Refactor download.py

--- backend/api/receipts/download.py
+++ backend/api/receipts/download.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.core.files.storage import default_storage
 from backend.models import Receipt, OneTimeURL
 
+
 def generate_download_url(request, receipt_id):
     # Retrieve the receipt from the database
     receipt = get_object_or_404(Receipt, pk=receipt_id)
@@ -16,7 +17,8 @@ def generate_download_url(request, receipt_id):
     OneTimeURL.objects.create(receipt=receipt, token=token)
 
     # Return the download URL
-    return JsonResponse({'url': receipt.get_receipt_url()})
+    return JsonResponse({"url": receipt.get_receipt_url()})
+
 
 def receipt_download(request, token):
     # Retrieve the one-time URL from the database
