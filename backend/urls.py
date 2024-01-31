e0c18566ee326e2faab7677cac955a739220b31b
Trey <treyww2008@hotmail.com>
2024-01-31 21:46:15 +0000

    Made a basic RestAPI for fetching clients with token base auth

--- backend/urls.py
+++ backend/urls.py
@@ -204,7 +204,7 @@ schema_view = get_schema_view(
 
 urlpatterns += [
     path(
-        "/api/docs/<format>/",
+        "api/docs/<format>/",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
