91a09e3090bd9dbcc704328773532119e93cd5ff
Trey <73353716+TreyWW@users.noreply.github.com>
2024-02-11 20:17:37 +0000

    Enhance/production (#199)
    * Updated prod_settings databases
    
    * Testing GH Actions
    
    * Testing GH Actions
    
    * Testing GH Actions
    
    * Testing GH Actions
    
    * Testing GH Actions
    
    * Testing GH Actions
    
    * Testing GH Actions
    
    * Changed to "release"
    
    * Ran black formatter
    
    * Setting to "latest" for now
    
    * Now only on tags (not just releases), and push two versions (latest and version)
    
    * ran black formatter

--- settings/prod_settings.py
+++ settings/prod_settings.py
@@ -1,4 +1,7 @@
-import os, environ
+import os
+from pathlib import Path
+
+import environ
 
 env = environ.Env()
 environ.Env.read_env()
@@ -9,20 +12,34 @@ CSRF_TRUSTED_ORIGINS = [
     f'https://{os.environ.get("URL")}',
     f'https://{os.environ.get("PROXY_IP")}',
 ]
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DB_TYPE = os.environ.get("DATABASE_TYPE")
+DB_TYPE = DB_TYPE.lower() if DB_TYPE else ""  # sqlite is disabled for production
+
+DB_TYPE = "mysql" if DB_TYPE in ["mysql", "mariadb"] else DB_TYPE
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DATABASE_NAME"),
-        "USER": os.environ.get("DATABASE_USER"),
-        "PASSWORD": os.environ.get("DATABASE_PASS"),
-        "HOST": os.environ.get("DATABASE_HOST"),
-        "PORT": os.environ.get("DATABASE_PORT") or 3306,
+        "ENGINE": (
+            "django.db.backends.postgresql_psycopg2"
+            if DB_TYPE == "mysql"
+            else "django.db.backends.postgresql"
+        ),
+        "NAME": os.environ.get("DATABASE_NAME") or "myfinances_development",
+        "USER": os.environ.get("DATABASE_USER") or "root",
+        "PASSWORD": os.environ.get("DATABASE_PASS") or "",
+        "HOST": os.environ.get("DATABASE_HOST") or "localhost",
+        "PORT": os.environ.get("DATABASE_PORT")
+        or (3306 if DB_TYPE == "mysql" else 5432),
         "OPTIONS": {
             "sql_mode": "traditional",
         },
     }
 }
+
+print(f"[BACKEND] Using {DB_TYPE} database: {os.environ.get('DATABASE_NAME')}")
+
 
 ALLOWED_HOSTS = [os.environ.get("URL")]
 
