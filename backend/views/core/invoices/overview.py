6eac80acb0dd1db7591afaca2ea0b97e12b12466
Trey <treyww2008@hotmail.com>
2024-03-16 22:25:37 +0000

    Added more control options for invoice overview

--- backend/views/core/invoices/overview.py
+++ backend/views/core/invoices/overview.py
@@ -1,5 +1,4 @@
 from django.http import HttpRequest
-from django.shortcuts import render
 
 from backend.decorators import *
 from backend.models import *
@@ -14,10 +13,10 @@ def invoices_dashboard(request: HttpRequest):
 def manage_invoice(request: HttpRequest, invoice_id: str):
     if not invoice_id.isnumeric():
         messages.error(request, "Invalid invoice ID")
-        return redirect("invoices dashboard")
+        return redirect("invoices:dashboard")
 
     invoice = Invoice.objects.get(id=invoice_id)
 
     if not invoice:
-        return redirect("invoices dashboard")
+        return redirect("invoices:dashboard")
     return render(request, "pages/invoices/dashboard/manage.html", {"invoice": invoice})
