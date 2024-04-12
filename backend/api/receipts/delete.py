c26cafda5e7a08da2d54077fbb5758803ccd308d
atulanand25 <48369355+atulanand25@users.noreply.github.com>
2024-04-13 01:08:20 +0530

    Bug fixed when creating receipt and deleting the receipts  (#326)
    * Issue - Bug fixed when creating receipt and deleting the receipts
    
    Description:
    I have validated and tweaked the creation and deletion of the receipts to ensure it works  as expected.
    
    Added the has_access method in the modal for receipts to use it when deleting the receipts
    
    * Update delete.py
    
    Error handling when reading from database for receipts
    
    * Ran Black Formatter
    
    ---------
    
    Authored-by: atanand2 <atanand2@cisco.com>

--- backend/api/receipts/delete.py
+++ backend/api/receipts/delete.py
@@ -1,31 +1,37 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest, JsonResponse
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse, HttpResponse, HttpResponseRedirect, QueryDict
+from django.shortcuts import render, redirect
+from django.urls import resolve, Resolver404, reverse
 from django.views.decorators.http import require_http_methods
-
 from backend.models import Receipt
 
 
 @require_http_methods(["DELETE"])
 @login_required
 def receipt_delete(request: HttpRequest, id: int):
-    receipt = Receipt.objects.filter(id=id).first()
+    try:
+        receipt = Receipt.objects.get(id=id)
+    except Receipt.DoesNotExist:
+        return JsonResponse({"message": "Receipt not found"}, status=404)
+
     if not receipt:
         return JsonResponse(status=404, data={"message": "Receipt not found"})
 
-    if request.user.logged_in_as_team and receipt.organization != request.user.logged_in_as_team:
-        return JsonResponse(status=403, data={"message": "Forbidden"})
-    elif receipt.user != request.user:
-        return JsonResponse(status=403, data={"message": "Forbidden"})
-
-    # QuotaLimit.delete_quota_usage("receipts-count", request.user, receipt.id, receipt.date_uploaded) # Don't want to delete receipts
-    # from records because it does cost us PER receipt. So makes sense not to allow Upload, delete, upload .. etc
+    if not receipt.has_access(request.user):
+        return JsonResponse({"message": "You do not have permission to delete this invoice"}, status=404)
 
     receipt.delete()
-    messages.success(request, "Receipt deleted")
-    return render(
-        request,
-        "pages/receipts/_search_results.html",
-        {"receipts": Receipt.objects.filter(user=request.user).order_by("-date")},
-    )
+    messages.success(request, f"Receipt deleted with the name of {receipt.name}")
+    if request.user.logged_in_as_team:
+        return render(
+            request,
+            "pages/receipts/_search_results.html",
+            {"receipts": Receipt.objects.filter(organization=request.user.logged_in_as_team).order_by("-date")},
+        )
+    else:
+        return render(
+            request,
+            "pages/receipts/_search_results.html",
+            {"receipts": Receipt.objects.filter(user=request.user).order_by("-date")},
+        )
