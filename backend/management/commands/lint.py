00db8bc244618bbd268ec2f5af83c3cbf8ccdf78
Trey <73353716+TreyWW@users.noreply.github.com>
2024-04-26 19:38:53 +0100

    enhance: Improved invoice details
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- backend/management/commands/lint.py
+++ backend/management/commands/lint.py
@@ -1,6 +1,7 @@
 import subprocess
 
 from django.core.management import BaseCommand
+from django.utils.termcolors import colorize
 
 
 class Command(BaseCommand):
@@ -9,13 +10,18 @@ class Command(BaseCommand):
     requires_migrations_checks = False
 
     def add_arguments(self, parser):
-        parser.add_argument("action", type=str, help="djlint or black")  # parser.add_argument("-f", type=str, dest="flag",
+        parser.add_argument("action", type=str, nargs="?", help="djlint or black")  # parser.add_argument("-f", type=str, dest="flag",
 
     def handle(self, *args, **kwargs):
         if kwargs["action"] == "djlint":
             djlint()
         elif kwargs["action"] == "black":
             black()
+        else:
+            self.stdout.write(colorize("Linting with: BLACK FORMATTER", fg="green", opts=("bold",)))
+            black()
+            self.stdout.write(colorize("Linting with: DJLINT", fg="green", opts=("bold",)))
+            djlint()
 
 
 def djlint():
