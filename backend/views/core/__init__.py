6b7979ed97f9407601684ecf93228ce9f9bab7ec
domejko <sllawny@gmail.com>
2024-04-30 16:51:45 +0200

    mypy errors fix

--- backend/views/core/__init__.py
+++ backend/views/core/__init__.py
@@ -2,6 +2,6 @@ from .auth.passwords import set
 from .other import index, errors
 from .auth import login
 from .settings import view as settings_view, teams
-from .invoices import dashboard as invoices_dashboard, create_invoice, view as invoices_view, manage_access
+from .invoices import dashboard as invoices_dashboard, create, view as invoices_view, manage_access
 from .clients import dashboard as clients_dashboard, create
 from .receipts import dashboard as receipts_dashboard
