6b7738a2a1480f6c83f8e24857895677a1085ec8
Trey <73353716+TreyWW@users.noreply.github.com>
2024-05-04 11:25:34 +0100

    fixed linting

--- tests/handler.py
+++ tests/handler.py
@@ -39,7 +39,7 @@ def create_mock_image():
     return SimpleUploadedFile("mock_image.jpg", image_io.getvalue(), content_type="image/jpeg")
 
 
-@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+@override_settings(STATICFILES_STORAGE="django.contrib.staticfiles.storage.StaticFilesStorage")
 class ViewTestCase(TestCase):
     def setUp(self):
         self.log_in_user = User.objects.create_user(username="user@example.com", password="user", email="user@example.com")
