571ca1fc940a18e866ac5748bb98dcfeabe44ad3
Trey <73353716+TreyWW@users.noreply.github.com>
2024-06-28 18:23:08 +0100

    removed fake migration from entrypoint
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- infrastructure/backend/scripts/entrypoint.sh
+++ infrastructure/backend/scripts/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-python3 manage.py migrate backend 0036_alter_client_address_clientdefaults --fake
-
 python3 manage.py migrate --no-input && python3 manage.py collectstatic --no-input
 
 gunicorn settings.wsgi:application --bind 0.0.0.0:9012 --workers 2
