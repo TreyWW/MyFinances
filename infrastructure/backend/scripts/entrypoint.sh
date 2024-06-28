bd07bbab5c43eface7d78ae563d5f065ca28c854
Trey <73353716+TreyWW@users.noreply.github.com>
2024-06-28 21:08:14 +0100

    added echo to entrypoint
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- infrastructure/backend/scripts/entrypoint.sh
+++ infrastructure/backend/scripts/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+echo "BEFORE MIGRATE"
 python3 manage.py migrate --no-input
-
+echo "BEFORE COLLECT STATIC"
 python3 manage.py collectstatic --no-input
-
+echo "BEFORE RUN APP"
 gunicorn settings.wsgi:application --bind 0.0.0.0:9012 --workers 2
