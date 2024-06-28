e79088fbf5b725135ae699a729fd174b9c096cec
Domejko <sllawny@gmail.com>
2024-06-28 17:15:07 +0200

    Added get to invoice api and updated naming convention

--- backend/api/public/endpoints/Invoices/urls.py
+++ backend/api/public/endpoints/Invoices/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import create, delete, edit, fetch
+from . import create, list, get
 
 urlpatterns = [
     path(
@@ -8,19 +8,34 @@ urlpatterns = [
         create.create_invoice_endpoint,
         name="create",
     ),
+    # path(
+    #     "delete/",
+    #     delete.delete_invoice_endpoint,
+    #     name="delete",
+    # ),
+    # path(
+    #     "edit/",
+    #     edit.edit_invoice_endpoint,
+    #     name="edit",
+    # ),
+    # path(
+    #     "edit/<int:invoice_id>/set_status/<str:status>/",
+    #     edit.change_status_endpoint,
+    #     name="edit status"
+    # ),
+    # path(
+    #     "edit/<str:invoice_id>/discount/",
+    #     edit.edit_discount_endpoint,
+    #     name="edit discount"
+    # ),
     path(
-        "delete/",
-        delete.delete_invoice_endpoint,
-        name="delete",
-    ),
+        "list/",
+        list.list_invoices_endpoint,
+        name="list"),
     path(
-        "edit/",
-        edit.edit_invoice_endpoint,
-        name="edit",
-    ),
-    path("edit/<int:invoice_id>/set_status/<str:status>/", edit.change_status_endpoint, name="edit status"),
-    path("edit/<str:invoice_id>/discount/", edit.edit_discount_endpoint, name="edit discount"),
-    path("fetch/", fetch.fetch_all_invoices_endpoint, name="fetch"),
+        "<str:id>/",
+        get.get_invoices_endpoint,
+        name="get"),
 ]
 
 app_name = "invoices"
