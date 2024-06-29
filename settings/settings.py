d0eb8ae2714e8fe36dcb028b39d8bfeb709c29c1
Trey <73353716+TreyWW@users.noreply.github.com>
2024-06-29 21:12:47 +0100

    updated ratelimit to 1 every 2 seconds
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- settings/settings.py
+++ settings/settings.py
@@ -72,7 +72,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_THROTTLE_CLASSES": ["rest_framework.throttling.AnonRateThrottle", "rest_framework.throttling.UserRateThrottle"],
-    "DEFAULT_THROTTLE_RATES": {"user": "250/hour", "anon": "250/day"},
+    "DEFAULT_THROTTLE_RATES": {"user": "1800/hour", "anon": "250/day"},
 }
 
 SWAGGER_SETTINGS = {
