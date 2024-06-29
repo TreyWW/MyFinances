2be26fbb36a061478a8c9ff315b3172cce2e0d44
Trey <73353716+TreyWW@users.noreply.github.com>
2024-06-29 01:17:50 +0100

    added basic throttles
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- settings/settings.py
+++ settings/settings.py
@@ -70,6 +70,8 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": (["rest_framework.renderers.JSONRenderer"]),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_THROTTLE_CLASSES": ["rest_framework.throttling.AnonRateThrottle", "rest_framework.throttling.UserRateThrottle"],
+    "DEFAULT_THROTTLE_RATES": {"user": "250/hour", "anon": "250/day"},
 }
 
 SWAGGER_SETTINGS = {
