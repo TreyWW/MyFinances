4135f9b5f8c9deb1ab83351823e10580af6fa08f
Trey <73353716+TreyWW@users.noreply.github.com>
2024-07-19 22:44:54 +0100

    fix: allow defaults to work with organisations
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- backend/service/defaults/get.py
+++ backend/service/defaults/get.py
@@ -4,6 +4,8 @@ from backend.types.requests import WebRequest
 
 def get_account_defaults(request: WebRequest, client: Client | None = None) -> DefaultValues:
     if not client:
-        account_defaults, _ = DefaultValues.objects.get_or_create(user=request.user, client=None)
+        account_defaults: DefaultValues
+        if not (account_defaults := DefaultValues.filter_by_owner(owner=request.actor).filter(client__isnull=True).first()):
+            account_defaults = DefaultValues.objects.create(owner=request.actor, client=None)
         return account_defaults
     return DefaultValues.filter_by_owner(owner=request.actor).get(client=client)
