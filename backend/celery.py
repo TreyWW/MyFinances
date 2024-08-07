c300549c1be2312ed12b09d4d6f9da2f50548d4e
Trey <73353716+TreyWW@users.noreply.github.com>
2024-08-07 23:37:35 +0100

    fixed circular import
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- backend/celery.py
+++ backend/celery.py
@@ -7,12 +7,10 @@ import sys
 from celery import Celery
 from django.conf import settings
 
-from settings.helpers import get_var
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
 
-CELERY_BROKER = get_var("CELERY_BROKER") or get_var("REDIS_CACHE_HOST") or "redis://localhost:6379/0"
-CELERY_BACKEND = get_var("CELERY_BACKEND") or get_var("REDIS_CACHE_HOST") or "redis://localhost:6379/0"
+CELERY_BROKER = os.environ.get("CELERY_BROKER") or os.environ.get("REDIS_CACHE_HOST") or "redis://localhost:6379/0"
+CELERY_BACKEND = os.environ.get("CELERY_BACKEND") or os.environ.get("REDIS_CACHE_HOST") or "redis://localhost:6379/0"
 
 app = Celery("backend", broker=CELERY_BROKER, backend=CELERY_BACKEND)
 
