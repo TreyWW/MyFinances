cab0771a77b7c2a36c2c6597e0fcf9d987dcc53c
Trey <73353716+TreyWW@users.noreply.github.com>
2024-08-07 12:38:59 +0100

    fix type for get weekly cron
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- backend/service/invoices/recurring/schedules/date_handlers.py
+++ backend/service/invoices/recurring/schedules/date_handlers.py
@@ -26,7 +26,7 @@ def get_monthly_cron(day_of_month: int | None = None, date: Date | None = None) 
 
 
 def get_weekly_cron(day_of_week: int | None = None) -> WeeklyCronServiceResponse:
-    if 0 <= day_of_week <= 6:
+    if isinstance(day_of_week, int) and 0 <= day_of_week <= 6:
         cron_expression = f"0 7 ? * {day_of_week} *"
         return WeeklyCronServiceResponse(True, cron_expression)
     else:
