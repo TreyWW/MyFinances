098883c751745c12e2c9a13f6f54c715803a2373
Trey <73353716+TreyWW@users.noreply.github.com>
2024-08-07 23:32:46 +0100

    added celery env variables
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- backend/celery.py
+++ backend/celery.py
@@ -7,9 +7,14 @@ import sys
 from celery import Celery
 from django.conf import settings
 
+from settings.helpers import get_var
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
 
-app = Celery("backend")
+CELERY_BROKER = get_var("CELERY_BROKER") or get_var("REDIS_CACHE_HOST") or "redis://localhost:6379/0"
+CELERY_BACKEND = get_var("CELERY_BACKEND") or get_var("REDIS_CACHE_HOST") or "redis://localhost:6379/0"
+
+app = Celery("backend", broker=CELERY_BROKER, backend=CELERY_BACKEND)
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
