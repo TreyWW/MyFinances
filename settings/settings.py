52bd2b68ae74b65f76ab32b798075e167ccec703
Trey <73353716+TreyWW@users.noreply.github.com>
2024-08-31 23:31:15 +0100

    WIP make sure to change staticfiles in STORAGES
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- settings/settings.py
+++ settings/settings.py
@@ -422,6 +422,9 @@ else:
     STATIC_URL = f"/static/"
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
     STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+    STORAGES["staticfiles"] = {
+        "BACKEND": "settings.settings.CustomStaticStorage",
+    }
     logging.info(f"{STATIC_URL=} | {STATIC_ROOT=} | {STATICFILES_STORAGE=}")
 
 AWS_MEDIA_PUBLIC_ENABLED = get_var("AWS_MEDIA_PUBLIC_ENABLED", default="False").lower() == "true"
