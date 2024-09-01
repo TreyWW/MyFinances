e8cfbafdea4477b4df1f5a01e0bdaf20c55d396e
Trey <73353716+TreyWW@users.noreply.github.com>
2024-09-01 10:17:02 +0100

    added check for billing enabled before using URLs
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- backend/templatetags/feature_enabled.py
+++ backend/templatetags/feature_enabled.py
@@ -1,9 +1,27 @@
 from django import template
+from django.urls import NoReverseMatch
 
 from backend.models import User
 from backend.utils.feature_flags import get_feature_status
 
+from django.conf import settings
+
 register = template.Library()
+
+
+@register.simple_tag
+def has_module(module_str: str):
+    return module_str in settings.INSTALLED_APPS
+
+
+@register.simple_tag
+def safe_url(view_name, *args, **kwargs):
+    from django.urls import reverse
+
+    try:
+        return reverse(view_name, args=args, kwargs=kwargs)
+    except NoReverseMatch:
+        return None
 
 
 @register.simple_tag
