6d052cac3145983d18c5c0ac7792526ac2b309b0
Trey <73353716+TreyWW@users.noreply.github.com>
2024-09-08 20:02:46 +0100

    Feature/landing page (#476)
    * updated landing page + changed template colours
    
    ---------
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- backend/urls.py
+++ backend/urls.py
@@ -13,7 +13,7 @@ from backend.api.public.swagger_ui import get_swagger_ui, get_swagger_endpoints
 from backend.views.core import receipts
 from backend.views.core.invoices.single.view import view_invoice_with_uuid_endpoint
 from backend.views.core.other.index import dashboard
-from backend.views.core.other.index import index
+from backend.views.core.other.index import index, pricing
 from backend.views.core.quotas.view import quotas_list
 from backend.views.core.quotas.view import quotas_page
 from backend.views.core.quotas.view import view_quota_increase_requests
@@ -29,6 +29,7 @@ urlpatterns = [
     path("api/", include("backend.api.urls")),
     path("webhooks/", include("backend.webhooks.urls")),
     path("", index, name="index"),
+    path("pricing", pricing, name="pricing"),
     path("dashboard/", dashboard, name="dashboard"),
     path("dashboard/settings/", include("backend.views.core.settings.urls")),
     path("dashboard/teams/", include("backend.views.core.teams.urls")),
--- backend/views/core/other/index.py
+++ backend/views/core/other/index.py
@@ -1,9 +1,16 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from login_required import login_not_required
 
 
+@login_not_required
 def index(request: HttpRequest):
-    return render(request, "pages/index.html")
+    return render(request, "pages/landing/index.html")
+
+
+@login_not_required
+def pricing(request: HttpRequest):
+    return render(request, "pages/landing/pricing.html")
 
 
 def dashboard(request: HttpRequest):
