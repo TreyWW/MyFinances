00e57dfd6abaf1a73e4c1913822d80f2a19a003e
Trey <73353716+TreyWW@users.noreply.github.com>
2024-10-06 14:35:53 +0100

    fix: clients dropdown not taking into consideration organizations (#509)
    Signed-off-by: Trey <73353716+TreyWW@users.noreply.github.com>

--- backend/api/clients/fetch.py
+++ backend/api/clients/fetch.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 
 from backend.decorators import web_require_scopes
+from backend.models import Client
 from backend.service.clients.get import fetch_clients, FetchClientServiceResponse
 from backend.types.htmx import HtmxHttpRequest
+from backend.types.requests import WebRequest
 
 
 @require_http_methods(["GET"])
@@ -21,15 +23,15 @@ def fetch_all_clients(request: HtmxHttpRequest):
 
 @require_http_methods(["GET"])
 @web_require_scopes("clients:read", True, True)
-def fetch_clients_dropdown(request: HtmxHttpRequest):
+def fetch_clients_dropdown(request: WebRequest):
     if not request.htmx:
         return redirect("clients:dashboard")
 
     selected_client = request.GET.get("existing_client_id") or None
-    clients: FetchClientServiceResponse = fetch_clients(request)
+    clients = Client.filter_by_owner(request.actor).filter(active=True)
 
     return render(
         request,
         "pages/invoices/create/destinations/_view_clients_dropdown.html",
-        {"clients": clients.response, "selected_client": selected_client},
+        {"clients": clients, "selected_client": selected_client},
     )
