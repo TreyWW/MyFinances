70ef2fa7e928fd8fb9590ecb02b08de7aff10dd5
Gunin Wasan <guninwasan@gmail.com>
2024-11-09 23:07:43 -0500

    remove unused import

--- backend/core/api/public/decorators.py
+++ backend/core/api/public/decorators.py
@@ -2,7 +2,6 @@ from functools import wraps
 
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
 from rest_framework import status
 
 from backend.models import TeamMemberPermission, Organization, Client
