85aff372ac9a9f2ccecb13bb0ab83b70a6280f4e
Jason Fisher <jason.fishe@fotoware.com>
2024-12-13 20:31:22 +0100

    Added regen feature for api keys

--- backend/core/api/settings/api_keys.py
+++ backend/core/api/settings/api_keys.py
@@ -56,6 +56,49 @@ def generate_api_key_endpoint(request: WebRequest) -> HttpResponse:
     return http_response
 
 
+@require_http_methods(["POST"])
+@web_require_scopes("api_keys:write")
+def regenerate_api_key_endpoint(request: WebRequest, key_id: str) -> HttpResponse:
+    key: APIAuthToken | None = get_api_key_by_id(request.user.logged_in_as_team or request.user, key_id)
+
+    if not key:
+        messages.error(request, "API key not found")
+        return render(request, "base/toast.html")
+
+    delete_api_key(request, request.user.logged_in_as_team or request.user, key=key)
+
+    key_obj, new_key_response = generate_public_api_key(
+        request,
+        request.user.logged_in_as_team or request.user,
+        api_key_name=key.name,
+        permissions=key.scopes,
+        expires=key.expires,
+        description=key.description,
+        administrator_toggle=bool(key.administrator_service_type),
+        administrator_type=key.administrator_service_type,
+    )
+
+    if not key_obj:
+        messages.error(request, f"Failed to regenerate the API key: {new_key_response}")
+        return render(request, "base/toast.html")
+
+    messages.success(request, "API key regenerated successfully")
+
+    http_response = render(
+        request,
+        "pages/settings/settings/api_key_generated_response.html",
+        {
+            "raw_key": new_key_response,
+            "name": key.name,
+        },
+    )
+
+    http_response.headers["HX-Reswap"] = "beforebegin"
+    http_response.headers["HX-Retarget"] = 'div[data-hx-container="api_keys"]'
+
+    return http_response
+
+
 @require_http_methods(["DELETE"])
 def revoke_api_key_endpoint(request: WebRequest, key_id: str) -> HttpResponse:
     key: APIAuthToken | None = get_api_key_by_id(request.user.logged_in_as_team or request.user, key_id)
