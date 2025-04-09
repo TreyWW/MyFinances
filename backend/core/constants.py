d4714069e4a48d126cd4f06026c8d211f091c3d6
laramerdol <laramerdol@gmail.com>
2025-04-09 16:30:07 +0300

    Remone unnecessary constant definition

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -15,8 +15,3 @@ MAX_LENGTH_CURRENCY = 3  # Used for currency codes
 # Decimal field constants
 DECIMAL_MAX_DIGITS = 15
 DECIMAL_PLACES = 2
-
-# Common help texts
-STAFF_STATUS_HELP_TEXT = "Designates whether the user can log into this admin site."
-ACTIVE_STATUS_HELP_TEXT = "Designates whether this user should be treated as active. Unselect this instead of deleting accounts."
-GROUPS_HELP_TEXT = "The groups this user belongs to. A user will get all permissions granted to each of their groups."
